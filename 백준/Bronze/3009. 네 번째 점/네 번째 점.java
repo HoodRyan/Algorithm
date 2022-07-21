c9ec0797edb88861f76d5b39e33b2c199dd51d0a
GauelF <44666092+GauelF@users.noreply.github.com>
2022-07-21 10:30:20 +0900

    [Bronze III] Title: 네 번째 점, Time: 208 ms, Memory: 17808 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -1,7 +1,7 @@
-import java.util.*;
-public class Main{
-    public static void main(String[] args)
-    {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x1 = sc.nextInt();
         int y1 = sc.nextInt();
@@ -10,34 +10,15 @@ public class Main{
         int x3 = sc.nextInt();
         int y3 = sc.nextInt();
 
-        int a = 0;  //x좌표
-        int b = 0;  //y좌표
-
-        if(x1 == x2)
-        {
-            a = x3;
-        }else if(x2==x3)
-        {
-            a = x1;
-        }
-        else if(x3 == x1)
-        {
-            a = x2;
-        }
-
-        if(y1 == y2)
-        {
-            b = y3;
-        }else if(y2==y3)
-        {
-            b = y1;
-        }
-        else if(y3 == y1)
-        {
-            b = y2;
-        }
-
-        System.out.println(a +" "+b);
+        System.out.printf("%d %d",x1^x2^x3,y1^y2^y3);
+        //비트연산자 
+        //ex) x1 = 5 , x2= 5 , x3 = 7 일때
+        //x1^x2 -> x1 = 0101 , x2 = 0101 -> x1^x2 = 0000
+        //(x1^x2)^x3 -> (x1^x2) = 0000 , x3 = 0111 -> x1^x2^x3 = 0111
+        
+        //ex) x1 = 5 , x2= 7 , x3 = 5 일때
+        //x1^x2 -> x1 = 0101 , x2 = 0111 -> x1^x2 = 0010
+        //(x1^x2)^x3 -> (x1^x2) = 0010 , x3 = 0101 -> x1^x2^x3 = 0111
 
     }
 }
