2a6da326fbf5742ade3188b7b3b943e4cc60c9fd
태용 <44666092+HoodRyan@users.noreply.github.com>
2022-12-01 13:29:06 +0900

    [Bronze III] Title: 최댓값, Time: 236 ms, Memory: 18456 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[10][10];
+        int max = 0;
+        int x= 0;   //x좌표
+        int y= 0;   //y좌표
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                arr[i][j] = sc.nextInt();
+                if(max < arr[i][j]){
+                    max = arr[i][j];
+                    x = i;  
+                    y = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println((x+1) + " " + (y+1));
+    }
+}
