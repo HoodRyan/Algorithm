24ad013f1d3c88db18513575b31849055dc69baf
태용 <44666092+HoodRyan@users.noreply.github.com>
2022-12-02 10:54:28 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        long sum  = 0;
+        for(long i=1;i<num;i++)
+        {
+            sum += (num+1)*i;
+        }
+
+        System.out.println(sum);
+
+    }
+}
