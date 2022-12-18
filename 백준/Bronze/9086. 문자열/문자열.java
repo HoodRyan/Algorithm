856216aa1eb716883363462e09acad4faead0571
태용 <44666092+HoodRyan@users.noreply.github.com>
2022-12-18 18:01:00 +0900

    [Bronze V] Title: 문자열, Time: 204 ms, Memory: 17476 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            String s = sc.next();
+            sb.append(s.charAt(0));
+            sb.append(s.charAt(s.length() - 1)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
