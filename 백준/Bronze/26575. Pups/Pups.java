98b2c4e66bb928f00dc12a88ccd8d4801d5de1af
태용 <44666092+HoodRyan@users.noreply.github.com>
2022-12-27 21:25:11 +0900

    [Bronze V] Title: Pups, Time: 140 ms, Memory: 14492 KB -BaekjoonHub

---
+++ 백준/Bronze/26575. Pups/Pups.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double d = Double.parseDouble(st.nextToken());
+            double f = Double.parseDouble(st.nextToken());
+            double p = Double.parseDouble(st.nextToken());
+            double sum = d*p*f;
+            String result = String.format("%.2f",sum);
+            System.out.println("$"+result);
+
+        }
+    }
+}
