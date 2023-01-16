dddc3ee9a1d6f072e59d339512a67fba95819b2b
태용 <44666092+HoodRyan@users.noreply.github.com>
2023-01-16 16:41:08 +0900

    [Silver II] Title: 최소 힙, Time: 332 ms, Memory: 26280 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> q = new PriorityQueue<>(); //내림차순 변경
+        for(int i=0;i<n;i++){
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0){ // 0이 주어진 횟수 
+                if(q.size() == 0){ //배열이 비어있는 경우인데 가장 큰 값을 출력 시
+                    sb.append(0).append('\n');
+                }else{ //배열에서 가장 큰 값을 출력 후 그 값을 제거.
+                    sb.append(q.poll()).append('\n');
+                }
+            }else{
+                q.add(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
