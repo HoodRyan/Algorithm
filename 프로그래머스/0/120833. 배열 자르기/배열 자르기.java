53a8268ceb4adc508052543536433fc4aab6cb4a
태용 <44666092+HoodRyan@users.noreply.github.com>
2026-04-10 11:57:44 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2-num1+1];
+        for(int i=0; num1<=num2; i++){
+            answer[i] = numbers[num1];
+            num1++;
+        }
+        return answer;
+    }
+}
